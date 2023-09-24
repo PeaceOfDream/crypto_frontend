ef151fd26b6865707b2360780f2e767159186ca3
Dima Bart <3088742@mail.ru>
2023-09-24 20:40:50 +0300

    add sidebar

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,24 +1,36 @@
-import React from 'react'
-import { ILayout } from '../../common/types/layout'
-import TopBarComponent from '../topBar'
-import { useLocation } from 'react-router-dom'
+import React, { useState } from 'react';
+import { ILayout } from '../../common/types/layout';
+import TopBarComponent from '../topBar';
+import { useLocation } from 'react-router-dom';
+import { Box, useMediaQuery } from '@mui/material';
+import SidebarComponent from '../sidebar';
 
-const LayoutComponent = ({children}: ILayout) => {
-	const location = useLocation()
+const LayoutComponent = ({ children }: ILayout) => {
+  const location = useLocation();
+  const isNonMobile = useMediaQuery('(min-width:600px)');
+	const [isOpen, setIsOpen] = useState(true)
 
-
-	
-	return location.pathname === '/login' ||
+  return location.pathname === '/login' ||
     location.pathname === '/register' ? (
-    <>
-      {children}
-    </>
+    <>{children}</>
   ) : (
-    <>
-      <TopBarComponent />
-      {children}
-    </>
+    <Box
+      display={isNonMobile ? 'flex' : 'block'}
+      width="100%"
+      height="100%"
+    >
+			<SidebarComponent
+				isNonMobile={isNonMobile}
+				drawerWidth='250px'
+				isOpen={isOpen}
+				setIsOpen={setIsOpen}
+			/>
+      <Box>
+        <TopBarComponent />
+        {children}
+      </Box>
+    </Box>
   );
-}
+};
 
-export default LayoutComponent
+export default LayoutComponent;
